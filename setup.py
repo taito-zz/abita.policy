39978430409214fdcccae5795a88785a93fcaf22
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-06-06 10:44:47 +0300

    release: 0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.policy',
-    version='0.8',
+    version='0.9',
     description="Turns Plone Site into Abita Site.",
     long_description=open("README.rst").read(),
     classifiers=[
